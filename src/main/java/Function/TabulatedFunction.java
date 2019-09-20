761b1415bffdaf5376c3c031dafd59a267866a8d
Maerodrim <stas.serg.99@mail.ru>
2019-09-20 11:21:53 +0400

    exp 2.4

--- src/main/java/Function/TabulatedFunction.java
+++ src/main/java/Function/TabulatedFunction.java
@@ -9,34 +9,31 @@ public class TabulatedFunction {
 
     TabulatedFunction(double leftX, double rightX, double[] values) {
         this.count = values.length;
-        this.xValues = Arrays.copyOf(xValues, count);
-        this.yValues = Arrays.copyOf(yValues, count);
+        this.yValues = Arrays.copyOf(values, count);
+        xValues = new double[count];
+        double buff = leftX;
+        double step = (rightX - leftX) / (count - 1);
+        if (leftX != rightX) {
+            for (int i = 0; i < count; i++) {
+                xValues[i] = buff;
+                buff += step;
+            }
+        }
     }
 
     TabulatedFunction(double leftX, double rightX, int pointsCount) {
         this.count = pointsCount;
-        if (xFrom > xTo) {
-            xFrom = xFrom - xTo;
-            xTo = xFrom + xTo;
-            xFrom = -xFrom + xTo;
-        }
         xValues = new double[count];
         yValues = new double[count];
-        double buff = xFrom;
-        double step = (xTo - xFrom) / (count - 1);
-        if (xFrom != xTo) {
+        double buff = leftX;
+        double step = (rightX - leftX) / (count - 1);
+        if (leftX != rightX) {
             for (int i = 0; i < count; i++) {
                 xValues[i] = buff;
-                yValues[i] = source.apply(buff);
-                buff += step;
-            }
-        } else {
-            double funcXFrom = source.apply(xFrom);
-            for (int i = 0; i < count; i++) {
-                xValues[i] = xFrom;
-                yValues[i] = funcXFrom;
+                yValues[i] = 0;
                 buff += step;
             }
         }
     }
 }
+
